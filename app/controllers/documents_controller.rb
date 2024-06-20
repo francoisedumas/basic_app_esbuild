6b92f35c922d6fa5364200b922b8dd276ddba093
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2024-06-20 17:27:22 +0200

    feat - Generate rubocop error for demonstration

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -6,7 +6,7 @@ class DocumentsController < ApplicationController
       format.html
       format.pdf do
         pdf = ::PdfGenerator::Base.generate_now(template: "documents/_table", locals: {})
-        send_data pdf, filename: "file.pdf"
+        send_data pdf, filename: 'file.pdf'
       end
     end
   end
