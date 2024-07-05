5a1169d64b587ae3c67b59f8e5c7d60fbae5644e
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2024-07-05 19:25:57 +0200

    Feat - Enable ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.routes.default_url_options = { host: "localhost", port: 3000 }
 
 Rails.application.configure do
+  config.hosts = (config.hosts rescue []) << /[-.\w]+\.ngrok-free\.app/
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
