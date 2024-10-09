26c33e9ad816672acfae100360c8ebf1b9653b71
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2024-10-09 15:18:59 +0200

    Feat - Webhook security

--- app/controllers/webhooks/dropbox_sign_controller.rb
+++ app/controllers/webhooks/dropbox_sign_controller.rb
@@ -2,10 +2,26 @@
 
 module Webhooks
   class DropboxSignController < BaseController
+    ALLOWED_IPS = [
+      "2600:1f18:20d2:4c00",
+      "34.200.190.67",
+      "44.216.219.123",
+      "34.198.205.50",
+      "184.73.232.209",
+      "3.229.107.48",
+      "34.198.117.22",
+      "2600:1f16:89:4700",
+      "3.17.43.141",
+      "13.59.145.12",
+      "3.135.245.223",
+      "3.23.150.114"
+    ].freeze
+
     EVENT_TYPE = {
       "contract" => DropboxSign::Webhooks::SignatureUpdate,
       "callback_test" => DropboxSign::Webhooks::CallbackTest
     }.freeze
+
     def create
       record = InboundWebhook.create(body: payload)
       kind = record.body.dig("signature_request", "metadata", "kind") || record.body.dig("event", "event_type")
@@ -22,8 +38,40 @@ module Webhooks
 
     private
 
+    def payload
+      @payload ||= begin
+        parsed_params = JSON.parse(params.require(:json))
+        ActionController::Parameters.new(parsed_params).
+          permit(
+            signature_request: [
+              :signature_request_id,
+              { metadata: [:kind] }
+            ],
+            event: [:event_type]
+          )
+      end
+    end
+
     def verify_event
-      nil
+      verify_ip_address
+      verify_secret
+    end
+
+    def verify_ip_address
+      render plain: "Not found", status: :not_found unless ALLOWED_IPS.include?(request.remote_ip)
+    end
+
+    def verify_secret
+      callback_data = JSON.parse(request.params[:json], symbolize_names: true)
+      callback_event = Dropbox::Sign::EventCallbackRequest.init(callback_data)
+
+      unless Dropbox::Sign::EventCallbackHelper.is_valid(api_key, callback_event)
+        render plain: "Not found", status: :not_found
+      end
+    end
+
+    def api_key
+      Rails.application.credentials.hellosign_api_key
     end
   end
 end
