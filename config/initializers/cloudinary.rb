b4f732c3b37919c987871531d900de8051a920c7
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2024-11-04 17:02:52 +0100

    move all the db related code on the rspec job

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-unless Rails.env.test?
-  Cloudinary.config do |config|
-    config.cloud_name = Rails.application.credentials.cloudinary[:cloud_name]
-    config.api_key = Rails.application.credentials.cloudinary[:api_key]
-    config.api_secret = Rails.application.credentials.cloudinary[:api_secret]
-    config.secure = true
-    config.cdn_subdomain = true
-  end
+return if Rails.env.test?
+
+Cloudinary.config do |config|
+  config.cloud_name = Rails.application.credentials.cloudinary[:cloud_name]
+  config.api_key = Rails.application.credentials.cloudinary[:api_key]
+  config.api_secret = Rails.application.credentials.cloudinary[:api_secret]
+  config.secure = true
+  config.cdn_subdomain = true
 end
