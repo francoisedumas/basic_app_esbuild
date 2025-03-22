8072a9e27e3444ffe0a82923a489c7441a9dd195
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2025-03-22 14:23:57 +0100

    Test VC

--- spec/components/definition_list_component_spec.rb
+++ spec/components/definition_list_component_spec.rb
@@ -3,13 +3,16 @@
 require "rails_helper"
 
 RSpec.describe DefinitionListComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "renders a definition list with entries" do
+    result = render_inline(described_class.new) do |component|
+      component.with_entry(term: "term 1", description: "description 1")
+      component.with_entry(term: "term 1", description: "description 1")
+    end
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+    expect(result.css("dl").count).to eq(1)
+    expect(result.css("dt").count).to eq(2)
+
+    expect(result.css("dt").first.to_html).to include("term 1")
+    expect(result.css("dd").first.to_html).to include("description 1")
+  end
 end
