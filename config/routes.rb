a8ecbc20d415f5ba5d67ab2807e58220e566a3be
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2025-05-20 21:07:28 +0200

    Basic lookbook

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount Lookbook::Engine, at: "/lookbook"
+  end
   devise_for :users
 
   direct :documentation do
